a933fd6a378f4e8a7fbe740b06716b7ca02c34bc
oriss7 <orissa37@gmail.com>
2026-04-13 15:31:36 +0300

    fix gmail smtp config

--- api/account/AccountController.js
+++ api/account/AccountController.js
@@ -34,7 +34,9 @@ async function signup(req, res) {
     } else {
       const nodemailer = require('nodemailer')
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.GMAIL_USER,
           pass: process.env.GMAIL_APP_PASSWORD
